8e2809c52c7cff2b39c8965a1f713b614bd35779
Gautam Padiyar <gautam.padiyar@gmail.com>
2017-10-22 16:01:19 +0530

    added basic gson parser logic

---
+++ src/Other/NetClientGet.java
@@ -0,0 +1,59 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class NetClientGet {
+
+    // http://localhost:8080/RESTfulExample/json/product/get
+    public static void main(String[] args) {
+
+        try {
+
+            URL url = new URL("https://java-final-proj.firebaseio.com/fests/Test.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            Gson sd = new Gson();
+
+            System.out.println(conn.toString());
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            StringBuilder sb = new StringBuilder();
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                sb.append(output);
+            }
+            Event ev = sd.fromJson(sb.toString(), Event.class);
+
+            System.out.println(ev.getStartDate());
+            System.out.println(ev.getEndDate());
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
